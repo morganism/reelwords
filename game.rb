e0cdcce4f3563b3ed7a5cfe9e215502fda5786b8
Morgan Sziraki <morgan.sziraki@gmail.com>
2024-03-28 15:36:18 +0000

    JOB-011 sync game

--- game.rb
+++ game.rb
@@ -3,4 +3,22 @@
 require_relative 'lib/trie'
 
 DATA_DIR = File.join(File.dirname(__FILE__), 'data')
+DICTIONARY = File.join(DATA_DIR, 'american-english-large.txt')
+REELS = File.join(DATA_DIR, 'reels.txt')
+SCORES = File.join(DATA_DIR, 'scores.txt')
 
+class Tile
+  def initialize(letter, value)
+    @letter = letter
+    @value = value
+  end
+end
+
+def readfile(filepath)
+  File.readlines(filepath).each do |line|
+    puts line
+  end
+end
+def load_scores
+
+end
